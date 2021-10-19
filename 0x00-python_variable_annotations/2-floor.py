2a556c6d988de6f82238085bbca3ccbbfc4e22dd
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-19 08:17:33 -0500

    ADD - float to string

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import math
-
 """ which takes a float n """
+
+import math
 
 
 def floor(n: float) -> int:
-    """ returns the floor value """
+    """ return the floor value """
     return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""that takes a float n as argument"""
+
+
+def to_str(n: float) -> str:
+    """returns a string representation of a number"""
+    return str(n)
