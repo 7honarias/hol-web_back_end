7e2a89d7da4bfb362be83e5c5d304582927e5308
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-18 22:08:06 -0500

    correction of documentations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+""" Basic annotations add """
+
+
 def add(a: float, b: float) -> float:
+    """ Add a float to a given number. """
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,8 @@
 #!/user/bin/env python3
 
+""" Basic annotations """
+
+
 def concat(a: str, b: str) -> str:
+    """return concatenations of two strings"""
     return a + b
