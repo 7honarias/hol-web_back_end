4cb17852df8951e0c177ff28707ab0946f017705
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-21 22:07:04 -0500

    ADD - task 7 complex type

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """sum mixed list int and float"""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_list: Union[int, float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """sum mixed list int and float"""
     return float(sum(mxd_list))
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""take string and a int return tuple"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """Take a string and a float return tuple"""
+    return (k, v)
