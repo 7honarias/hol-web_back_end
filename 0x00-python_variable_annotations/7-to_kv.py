b9c7d18244e1ccc77b8abbe505e2b6023292a0e0
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-21 22:10:23 -0500

    Correction task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,6 @@
 """take string and a int return tuple"""
 from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Take a string and a float return tuple"""
-    return (k, v)
+    return (k, v**2)
