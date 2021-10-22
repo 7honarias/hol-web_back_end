d4e124432ed1cc0857ab5cfbff3ab6e72cefcb97
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-21 22:35:29 -0500

    ADD - task 9 duck type iterable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,7 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a multiplier callable"""
     def multi(x: float):
         return multiplier * x
     return multi
