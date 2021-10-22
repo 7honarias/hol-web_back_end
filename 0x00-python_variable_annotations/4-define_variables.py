aaee34e9fad79dc93b3fecd0f6a53bdddd6ed632
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-21 21:41:17 -0500

    ADD - 5 sum list float

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Define variables to be used in the command line. """
+""" Define variables """
 
-a: int  = 1
+a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
