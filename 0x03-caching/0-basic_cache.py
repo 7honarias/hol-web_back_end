4ba31faf41a8f1647bf67a32791ee6106bbc01ca
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-24 12:49:28 -0500

    Correction errr pep8

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """ The basics of async """
 
@@ -12,7 +12,7 @@ class BasicCache(BaseCaching):
         """ Put a key/value pair """
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Get a key/value pair """
         return self.cache_data.get(key, None)
