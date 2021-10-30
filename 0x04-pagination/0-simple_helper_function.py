639ad567368985fc5057fd75f4a54367649db918
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-30 09:51:37 -0500

    Correction errors

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ simple helper function"""
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple:
     """ range index function """
     return ((page * page_size)-page_size, (page * page_size))
