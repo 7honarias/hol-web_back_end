6b4578c59e1d8c33090c99cace7a5220534331c1
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-30 09:49:30 -0500

    ADD - correction tuple

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -2,6 +2,6 @@
 """ simple helper function"""
 
 
-def index_range(page, page_size) -> tuple:
+def index_range(page, page_size):
     """ range index function """
     return ((page * page_size)-page_size, (page * page_size))
