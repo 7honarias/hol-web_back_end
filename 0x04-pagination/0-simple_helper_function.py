204d2b11a15e3b356d2db875d67333f11649f577
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-30 09:45:13 -0500

    ADD - simple helper function2

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" simple helper function"""
+
+
+def index_range(page, page_size) -> tuple:
+    """ range index function """
+    return ((page * page_size)-page_size, (page * page_size))
